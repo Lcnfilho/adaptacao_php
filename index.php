ed580310b01f3cc865f1473a1abac5581810f668
Luiz Carlos <44515810+Lcnfilho@users.noreply.github.com>
2019-12-19 15:57:38 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include_once("conexao.php");
 
 if(!$conn) {
@@ -16,17 +15,23 @@ if(!$conn) {
     </body>
         <h1>Cadastrar Produto</h1>
         <?php
-        if(isset($_SESSION['msg'])) {
-			echo $_SESSION['msg'];
-			unset($_SESSION['msg']);
-		}
 		$sql = "SELECT * FROM produtos";
 		$resultado = mysqli_query($conn, $sql);
-		echo "ID" . " | " . "Descrição" . " | " . "Unidade" . " | " . "Valor" . " | " . "Cadastrado em" . "<br><hr>";
-		while ($row = mysqli_fetch_assoc($resultado)) {
-			echo $row["id"] . " | " . $row["descricao"] . " | " . $row["unidade"] . " | " . $row["valor"] . " | " . $row["created"] . "<br><hr>";
+		if(mysqli_num_rows($resultado) > 0) {
+			echo "<table style='width 100%' border='1'>";
+			echo "<tr><th>ID</th>"
+				."<th>Descrição</th>"	
+				."<th>Unidade</th>"
+				."<th>Valor</th>"
+				."<th>Criado em</th>"
+				."</tr>";
+			while ($row = mysqli_fetch_array($resultado)) {
+				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["descricao"] . "</td><td>" . $row["unidade"] . "</td><td>" . $row["valor"] . "</td><td>" . $row["created"] . "</td></tr>";
+			}
+		} else {
+			echo "Nenhum produto cadastrado";
 		}
-		echo "<a href='cadastro.php'>Cadastrar</a>";
+		echo "<a href='cadastro.php'>Cadastrar</a><hr>";
         ?>
     </body>
 </html>
