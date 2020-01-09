b01ed958e136dfc0b9aec231c4e2cddf4981acce
Luiz Carlos <44515810+Lcnfilho@users.noreply.github.com>
2020-01-09 15:03:57 -0300

    Add files via upload

--- produtos.php
+++ produtos.php
@@ -15,16 +15,37 @@ $valor_unitario = filter_input(INPUT_POST,'valor_unitario', FILTER_SANITIZE_STRI
 //echo "Descrição: $descricao <br>";
 //echo "Unidade: $unidade <br>";
 //echo "Valor unitário: $valor_unitario <br>";
-
-$sql = "INSERT INTO produtos (descricao, unidade, valor, created) VALUES ('$descricao', '$unidade', '$valor_unitario', NOW())";
-$resultado = mysqli_query($conn, $sql);
-
-if (mysqli_insert_id($conn)) {
-	$_SESSION['msg'] = "<p style='color:green;'>Produto cadastrado com sucesso!</p>";
-    header("Location: cadastro.php");
+$query_select = "SELECT descricao FROM produtos WHERE descricao = '$descricao'";
+$select = mysqli_query($query_select,$conn);
+$array = mysqli_fetch_array($select);
+$descricao_array = $array['descricao'];
+if(empty($descricao)) {
+	$_SESSION['msg'] = "<p style='color:red;'>O campo Descrição não pode estar em branco</p>";
+	header("Location: cadastro.php");
 } else {
-	$_SESSION['msg'] = "<p style='color:red;'>Produto não pode ser cadastrado!</p>";
-    header("Location: cadastro.php");
+	if($descricao_array == $descricao) {
+		$_SESSION['msg'] = "<p style='color:red;'>Produto já cadastrado</p>";
+		header("Location: cadastro.php");
+	} else {
+		if(empty($valor_unitario)) {
+			$_SESSION['msg'] = "<p style='color:red;'>O campo Valor não pode estar em branco</p>";
+			header("Location: cadastro.php");
+		} else if(!is_numeric($valor_unitario)) {
+			$_SESSION['msg'] = "<p style='color:red;'>Valor Unitário inválido: Digite apenas números e ponto para separa-los</p>";
+			header("Location: cadastro.php");
+		} else {
+			$sql = "INSERT INTO produtos (descricao, unidade, valor, created) VALUES ('$descricao', '$unidade', '$valor_unitario', NOW())";
+			$resultado = mysqli_query($conn, $sql);
+
+			if (mysqli_insert_id($conn)) {
+				$_SESSION['msg'] = "<p style='color:green;'>Produto cadastrado com sucesso</p>";
+				header("Location: index.php");
+			} else {
+				$_SESSION['msg'] = "<p style='color:red;'>Produto não pode ser cadastrado</p>";
+				header("Location: cadastro.php");
+			}
+		}
+	}
 }
 mysqli_close($conn);
 
